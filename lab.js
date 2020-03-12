6c36764d31aefe8fd94045740fed714a237237b3
filosofeTHElama <jeremy.lelay@outlook.fr>
2020-03-12 15:58:30 +0000

    Job's done

---
+++ lab.js
@@ -0,0 +1,24 @@
+//Q4
+function myAlphabetLength () {
+    var myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    console.log(myAlphabet.length);
+}
+
+//Q5
+var Planets;
+Planets = ['Mars', 'Venus', 'Earth', 'Mercury', 'Neptune'];
+
+//Q6
+Planets.forEach(e => console.log(e));
+
+//Q7
+var wowDatatypes = [1, 2.9, '3'];
+wowDatatypes.forEach(e => console.log(e));
+
+//Q11
+var hello = function(){
+    return 'Hello world!';
+}
+
+//Q12
+console.log(hello());
